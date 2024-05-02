a168651d3c1c7ad6274b7e65338beb5218c96f80
Paolo Pastacaldi <paolo.pastacaldi@knobs.it>
2024-05-02 15:09:20 +0200

    feat: new header + minor fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,76 +1,62 @@
-
 import Drawer from "react-modern-drawer";
-import 'react-modern-drawer/dist/index.css'
+import "react-modern-drawer/dist/index.css";
 import DrawerMenu from "./DrawerContent";
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import { isDrawerMenuOpen } from "../stores/drawerMenuStore";
 import { useStore } from "@nanostores/react";
 
-import logo from '../images/logo.svg'
+import logo from "../images/logo.svg";
 
-const Header = ()  => {
-
+const Header = () => {
   const $isMenuOpen = useStore(isDrawerMenuOpen);
-  const toggleDrawer = () => isDrawerMenuOpen.set(!$isMenuOpen)
+  const toggleDrawer = () => isDrawerMenuOpen.set(!$isMenuOpen);
 
   return (
-    <header className="h-[72px] font-serif flex items-center justify-between bg-lightGreen text-secondary">
-        <div className="flex items-center justify-between w-full mx-8">
-          <img src={logo.src} />
+    <header className="relative h-[80px] font-sans mx-8 flex items-center justify-between bg-white text-darkBlue">
+      <img className="" src={logo.src} />
 
-          {/* MOBILE */}
-            <div className="lg:hidden">
-              <button onClick={toggleDrawer}>
-                <GiHamburgerMenu size="1.5rem" />
-              </button>
-              <Drawer
-                open={$isMenuOpen}
-                direction="left"
-                customIdSuffix="drawer"
-              >
-                <DrawerMenu toggleDrawer={toggleDrawer}  />
-              </Drawer>
-            </div>
-        
+      {/* MOBILE */}
+      <div className="lg:hidden">
+        <button onClick={toggleDrawer}>
+          <GiHamburgerMenu size="1.5rem" />
+        </button>
+        <Drawer open={$isMenuOpen} direction="left" customIdSuffix="drawer">
+          <DrawerMenu toggleDrawer={toggleDrawer} />
+        </Drawer>
+      </div>
 
-          {/* DESKTOP */}
-            <div className="hidden lg:block">
-              <nav>
-                <ul className="list-none flex gap-5 text-xl">
-                  <li>
-                    <a
-                      aria-label="Vai alla sezione specialisti"
-                      href="#specialisti"
-                    >
-                      SPECIALISTI
-                    </a>
-                  </li>
-                  <li>
-                    <a aria-label="Vai alla sezione dove siamo " href="#dove-siamo">
-                      DOVE SIAMO
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      aria-label="Vai alla sezione contatti "
-                      href="#contatti"
-                    >
-                      CONTATTI
-                    </a>
-                  </li>
-                  <li>
-                    <a aria-label="Vai alla sezione faq " href="#faq">
-                      FAQ
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-        
-        </div>
+      {/* DESKTOP */}
+      <nav className="hidden lg:flex items-center justify-between absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 pr-16 2xl:pr-0">
+        <ul className="list-none inline-flex items-center gap-5 text-xl">
+          <li>
+            <a aria-label="Vai alla sezione specialisti" href="#specialisti">
+              Home
+            </a>
+          </li>
+          <li>
+            <a aria-label="Vai alla sezione dove siamo " href="#dove-siamo">
+              Dove siamo
+            </a>
+          </li>
+          <li>
+            <a aria-label="Vai alla sezione contatti " href="#contatti">
+              Contatti
+            </a>
+          </li>
+          <li>
+            <a aria-label="Vai alla sezione faq " href="#faq">
+              FAQ
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex gap-6">
+        <button className="white-cta">TROVA LO SPECIALISTA</button>
+        <button className="blue-cta lg:hidden xl:block">CHIAMA ORA</button>
+      </div>
     </header>
-)
-}
+  );
+};
 
 export default Header;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
     colors: {
-      primary: "#6BD697",
-      lightGreen: "#CEF1DC",
-      secondary: "#21137F",
-      white: "#FFFFFF",
-      black: "#000000",
-      lightblue: "lightblue",
+      primary: "#2e9370",
+      primaryLight: "#bce5d7",
+      secondary: "#000c64",
+      secondaryLight: "#cccee0",
+      tertiary: "#fc8421",
+      secondaryLighter: "#8d8c9b",
+      darkBlue: "#1b1937",
+      lightGrey: " #f2f2f3",
+      grey: "#d1d1d7",
+      ...colors,
     },
     fontFamily: {
       serif: ["Bebas Neue", ...defaultTheme.fontFamily.serif],
