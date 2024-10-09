7e999c3b69edbaa9428d15526840c283c82d84b1
Paolo Pastacaldi <paolopastacaldi.dev@gmail.com>
2024-10-09 15:19:24 +0200

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+  site: "https://centromedicoagliana.it",
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), sitemap()]
 });
