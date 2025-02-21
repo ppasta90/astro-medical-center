f1fa43a0ade17e9f7324e7ef58321a6833ad650e
Paolo Pastacaldi <paolopastacaldi.dev@gmail.com>
2025-02-21 20:41:29 +0100

    modified sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://centromedicoagliana.it",
+  site: "https://www.centromedicoagliana.it",
   integrations: [tailwind({
     applyBaseStyles: false
   }), react(), sitemap(), partytown()]
